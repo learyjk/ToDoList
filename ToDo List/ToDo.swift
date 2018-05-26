e7fa50169b90ea44607aaa8b1ff785bf70cc8940
Keegan Leary <leary.keegan@gmail.com>
2018-05-26 17:07:36 -0400

    Created ToDo class

---
+++ ToDo List/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDo List
+//
+//  Created by Keegan Leary on 5/26/18.
+//  Copyright © 2018 Keegan Leary. All rights reserved.
+//
+
+import UIKit
+class ToDo {
+    var name = ""
+    var important = false
+}

c448ff2a9437ad6736268e055003d6d272e68499
Unknown <leary.keegan@gmail.com>
2018-05-28 08:15:45 -0400

    Fun with unwrapping
    changed unwrapping styles

--- ToDo List/ToDoTableViewController.swift
+++ ToDo List/ToDoTableViewController.swift
@@ -50,8 +50,9 @@ class ToDoTableViewController: UITableViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let addVC = segue.destination as! AddToDoViewController
-        addVC.previousVC = self
+        if let addVC = segue.destination as? AddToDoViewController {
+            addVC.previousVC = self
+        }
     }
 
 }
